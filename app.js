ffef9a8aef93716c5ada9c623a30aac056c33e63
Joey Dang <jodang@ucsd.edu>
2021-02-14 16:37:39 -0800

    Add Google Analytics

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-
 /**
- * Module dependencies.
- */
-
+* Module dependencies.
+*/
+ 
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
-
+ 
 var index = require('./routes/index');
 var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
-
+ 
 var app = express();
-
+ 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -29,18 +28,19 @@ app.use(express.cookieParser('Intro HCI secret key'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+ app.use(express.errorHandler());
 }
-
+ 
 // Add routes here
 app.get('/', index.view);
+app.get('/viewAlt', index.viewAlt);
 app.get('/project/:id', project.view);
 // Example route
 // app.get('/users', user.list);
-
+ 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+ console.log('Express server listening on port ' + app.get('port'));
 });
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,13 +1,19 @@
 'use strict';
-
+ 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+ initializePage();
 });
-
+ 
 /*
- * Function that is called when the document is ready.
- */
+* Function that is called when the document is ready.
+*/
 function initializePage() {
-	// your code here
+ // your code here
+ $('.likeBtn').click(clickLikeBtn);
+}
+ 
+function clickLikeBtn(e) {
+ e.preventDefault();
+ ga('send', 'event', 'like', 'click');
 }
